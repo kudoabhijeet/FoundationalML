cf704dffac731e870a400daba34ad26925f27b27
Abhijeet Prasad <abhi.prasad16@mgmail.com>
2019-01-31 20:25:36 +0530

    Features

--- ml1.py
+++ ml1.py
@@ -1,14 +1,19 @@
 import pandas as pd
-import quandl
+import quandl , math
 
 df = quandl.get('WIKI/GOOGL')
 
-#print (df.head)
+df = df[['Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume',]]
+df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100.0
+df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] *100.0
+df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
 
-print ('\n')
+forecast_col = 'Adj. Close'
+df.fillna(-9999, inplace =True) #Fill not available
 
-df['PCT'] = (df['Adj. High'] - df['Adj. Low'] / df['Adj. Low']) * 100.0
+forecast_out = int(math.ceil(0.01 * len(df)))
 
-df = df [['Adj. Open','Adj. High','Adj. Low', 'PCT']]
+df['label'] = df[forecast_col].shift(-forecast_out)
+df.dropna(inplace =True)
 
-print (df.head)
+print (df.head())
